b1e3cb37ab80ba0831b79b1e5ae7ac4b0cb41745
czk <2993944330@qq.com>
2024-02-15 17:25:39 +0800

    manage

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,12 @@ urlpatterns = [
     path('toregister/', views.toregister_view),
     path('register/', views.register_view),
     path('main/', views.main_page, name='main_page'),
-    path('main1/', views.main, name='main'),
     path('get_worldgeopark_data/', views.get_worldgeopark_data, name='get_worldgeopark_data'),
+    path('manage/', views.manage_page, name='manage_page'),
+    path('add/', views.add_page, name='add_page'),
+    path('search/', views.search_page, name='search_page'),
+    path('delete/', views.detele_page, name='detele_page'),
+    path('list/', views.list_page, name='list_page'),
+    path('toadd/', views.add_worldgeopark),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -47,10 +47,9 @@ def register_view(request):
 
 def main_page(request):
     # 主页面
-    return render(request, 'try.html')
+    return render(request, '首页.html')
 
-def main(request):
-    return render(request, 'main.html')
+
 
 def get_worldgeopark_data(request):
     # 从数据库中获取Worldgeopark数据
@@ -58,3 +57,41 @@ def get_worldgeopark_data(request):
 
     # 将数据转换为JSON格式并返回给前端
     return JsonResponse(list(worldgeopark_data), safe=False)
+
+def manage_page(request):
+    return render(request, 'manage.html')
+
+def add_page(request):
+    return render(request, 'add.html')
+
+def search_page(request):
+    return render(request, 'search.html')
+
+def detele_page(request):
+    return render(request, 'detele.html')
+
+def list_page(request):
+    return render(request, 'list.html')
+
+def add_worldgeopark(request):
+    if request.method == 'POST':
+        # 获取表单提交的数据
+        name = request.POST.get('name')
+        address = request.POST.get('address')
+        time = request.POST.get('time')
+        protect = request.POST.get('protect')
+        scene = request.POST.get('scene')
+
+        # 创建 Worldgeopark 对象并保存到数据库
+        worldgeopark = Worldgeopark(
+            name=name,
+            address=address,
+            time=time,
+            protect=protect,
+            scene=scene
+        )
+        worldgeopark.save()
+
+        # 重定向到成功页面或其他页面
+        return redirect('success_page')
+
